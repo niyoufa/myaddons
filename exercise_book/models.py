2de6324c902c70c004b5844b4edda16506b7565b
niyoufa <niyoufa@tmlsystem.com>
2016-06-12 00:17:08 +0800

    execute book

---
+++ exercise_book/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class exercise_book(models.Model):
+#     _name = 'exercise_book.exercise_book'
+
+#     name = fields.Char()
---
+++ training/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class training(models.Model):
+#     _name = 'training.training'
+
+#     name = fields.Char()
