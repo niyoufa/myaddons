37a9525a26500a59684ec981320e19e7797fd9da
niyoufa <niyoufa@tmlsystem.com>
2016-09-03 00:30:24 +0800

    pyc

--- schoolwork/__openerp__.py
+++ schoolwork/__openerp__.py
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'views/user_view.xml',
         'templates.xml',
     ],
     # only loaded in demonstration mode
